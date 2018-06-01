9fa1989fa17f814d1b8dec9828c292e68f08a0a4
sangmin <sang93423@gmail.com>
2018-06-01 17:02:25 +0900

    views.py 경로작성, post_list.html작성

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,5 +1,14 @@
+import os
+
 from django.http import HttpResponse
 
 
 def post_list(request):
-    return HttpResponse('<html><body>Post List</body></html>')
+    cur_file_path = os.path.abspath(__file__)
+    blog_dir_path = os.path.dirname(cur_file_path)
+    app_dir_path = os.path.dirname(blog_dir_path)
+    templates_dir_path = os.path.join(app_dir_path, 'templates')
+    blog_template_file_path = os.path.join(templates_dir_path, 'blog', 'post_list.html')
+    print(blog_template_file_path)
+    html = open(blog_template_file_path, 'rt').read()
+    return HttpResponse(html)
