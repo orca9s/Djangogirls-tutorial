2e1eadf55e612622cf200dc3001fcbe1d4fdd9fe
sangmin <sang93423@gmail.com>
2018-06-04 15:08:16 +0900

    post_list에서 동적으로 글 목록 문자열을 만들어 리턴해보기

--- app/blog/views.py
+++ app/blog/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import render_to_string
 
+from blog.models import Post
+
 
 def post_list(request):
     # cur_file_path = os.path.abspath(__file__)
@@ -17,4 +19,19 @@ def post_list(request):
     # 경로에 해당하는 HTML파일을 문자열로 로드해줌
     # html = render_to_string('blog/post_list.html')
     # 가져온 문자열을 돌려주기
-    return render(request, 'blog/post_list.html')
+    # return render(request, 'blog/post_list.html')
+    posts = Post.objects.all()
+    print(posts)
+    # Post instance에서 title속성에 접근가능
+    # HttpResponse에
+    #
+    # 글 목록
+    # - 격전 참여시...
+    # - 부정행위...
+    # - PBE...
+    #
+    # 위 텍스트를 넣어서 리턴
+    result = '글 목록<br>'
+    for post in Post.objects.all():
+        result +='{}<br>'.format(post.title)
+    return HttpResponse(result)
