f4a7a37c4b4e84ad29aaf35592419a0f504fa2e9
sangmin <sang93423@gmail.com>
2018-06-04 12:46:21 +0900

    HTML파일을 불러올 때 render_to_string 사용
    TEPLATES_DIR 지정 후 , 해당 경로를 settings모듈의 TEMPLATES['DIRS']에추가
    render_to_string에서 템플릿 파일을 찾기 위해 주어진 경로는 settings모듈의 TEMPLATES['DIRS']에 있는 폴더 목옭을 기준으로 검색한다.

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,14 +1,19 @@
 import os
 
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 
 def post_list(request):
-    cur_file_path = os.path.abspath(__file__)
-    blog_dir_path = os.path.dirname(cur_file_path)
-    app_dir_path = os.path.dirname(blog_dir_path)
-    templates_dir_path = os.path.join(app_dir_path, 'templates')
-    blog_template_file_path = os.path.join(templates_dir_path, 'blog', 'post_list.html')
-    print(blog_template_file_path)
-    html = open(blog_template_file_path, 'rt').read()
+    # cur_file_path = os.path.abspath(__file__)
+    # blog_dir_path = os.path.dirname(cur_file_path)
+    # app_dir_path = os.path.dirname(blog_dir_path)
+    # templates_dir_path = os.path.join(app_dir_path, 'templates')
+    # blog_template_file_path = os.path.join(templates_dir_path, 'blog', 'post_list.html')
+    # print(blog_template_file_path)
+    # html = open(blog_template_file_path, 'rt').read()
+
+    # 경로에 해당하는 HTML파일을 문자열로 로드해줌
+    html = render_to_string('blog/post_list.html')
+    # 가져온 문자열을 돌려주기
     return HttpResponse(html)
