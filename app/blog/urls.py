ad6b97de0e39f1969e412cde0b8aacd202727132
sangmin <sang93423@gmail.com>
2018-06-16 17:51:32 +0900

    포스트 기능별 연결 url주석 달아주기

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -11,10 +11,18 @@ urlpatterns = [
     # blog.views에 있는 post_list함수를
     # 아래 url함수의 두 번째 인자로 전달
     #   (함수호출 아님)
+
+    # 포스트 리스트 연결 url
     url(r'^$', post_list, name='post-list'),
+
+    # 포스트 디테일 연결 url
     url(r'^post/(\d+)/$', post_detail, name='post-detail'),
+
+    # 포스트 삭제 연결 url
     url(r'^post/(\d+)/delete/$', post_delete, name='post-delete'),
+    # 포스트 만들기 연결 url
     url(r'^write/', post_create, name='post-create'),
+    # 포스트 에딧 연결 url
     url(r'^post/(\d+)/edit/$', post_edit, name='post-edit'),
 
 ]
